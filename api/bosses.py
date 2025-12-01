6996ada234d14b979cd9b7987424d221f05e7974
Keenlyien <ritsunikki@gmail.com>
2025-12-01 14:25:26 +0800

    bosses api

--- api/bosses.py
+++ api/bosses.py
@@ -2,15 +2,31 @@ import os
 import json
 from pymongo import MongoClient
 
-MONGO_URI = os.environ.get("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client.bossTracker
-collection = db.bosses
-
 def handler(request):
-    bosses = list(collection.find({}, {"_id": 0}))  # exclude MongoDB _id
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"bosses": bosses})
-    }
+    try:
+        # Load connection string
+        MONGO_URI = os.environ.get("MONGO_URI")
+        if not MONGO_URI:
+            raise Exception("MONGO_URI is missing in Vercel environment variables")
+
+        # Connect
+        client = MongoClient(MONGO_URI)
+        db = client.bossTracker
+        collection = db.bosses
+
+        # Fetch data
+        bosses = list(collection.find({}, {"_id": 0}))
+
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"bosses": bosses})
+        }
+
+    except Exception as e:
+        # Return the full crash error
+        return {
+            "statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"error": str(e)})
+        }
